d5955e3e88470fac7a846bca47ec880fcd60d125
Dawud-Shuaibu <dawudshuaibu6565@gmail.com>
2024-10-23 14:04:47 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+/* JavaScript Reference Types
+
+1. Array.
+*/
+
+// Array
+let selectedColor = ['black', 'green'];
+
+// Changing a value of a data in Array
+selectedColor['0'] = 'red';
+
+// Adding a data in Array
+selectedColor['2'] = 'blue';
+
+console.log(selectedColor);
+
+// Logging one data in the console
+console.log(selectedColor['2']);
